705705bce1e73bb304d4382bb6b6f59747c2926e
tomatem <tem7720@gmail.com>
2023-05-27 19:27:48 +0300

    all done

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -33,22 +33,14 @@ export function reducer(state, action) {
     }
     case 'SAVE_EDITED_TASK': {
       const {id, editTask: task} = action.payload;
-      console.log(action.payload);
-      console.log('task');
-      console.log(task);
-      console.log("массив старый ")
-      console.log(state.toDoList);
       let newPosts = state.toDoList.map(el => {
         if (id === el.id) {
           el.task = task;
           el.taskCheckStatus = false;
           el.taskEditStatus = false;
         }
-        console.log("1 элемент " + el);
         return {...el};
       });
-      console.log("массив ")
-      console.log(newPosts);
       return {...state, toDoList: newPosts}
     }
     default: {
